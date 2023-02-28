0eb63d01647323f16566b9bb66792217dbc9b01c
candrafp <119491886+candrafp@users.noreply.github.com>
2023-02-28 01:07:44 -0800

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,17 @@
+<?php
+$leptop ="Lenovo";
+
+switch ($leptop) {
+        case'Lenovo';
+        echo "Lenovo core i7 dengan harga Rp14.000.000,00", PHP_EOL;
+        break;
+        case'ASUS';
+        echo"Asus vivobox core i7 dengan harga Rp5.599.000,00", PHP_EOL;
+        break;
+        case'ACER';
+        echo"Acer dengan harga Rp3.000.000,00", sPHP_EOL;
+        break;
+        default:
+        echo"leptop dengan kualitas terbaik dan kapasitas yang menjamin dapat mempermudah pekerjaan anda " .PHP_EOL;
+        break;
+}
---
+++ tugas2.php
@@ -0,0 +1,7 @@
+<?php
+
+$angka = "66";
+
+$hi = $angka % 2 == 0  ? "bilangan genap" : "bilangan ganji";
+
+echo $hi;
